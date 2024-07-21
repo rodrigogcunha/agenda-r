db71fba3e3c8bae72d1d782dec8f17789ed8beb1
Rodrigo Cunha <rodrigocunha@live.com>
2024-07-21 17:01:04 -0300

    commit

--- novo.js
+++ novo.js
@@ -1,6 +1,31 @@
-const endpoint = "http://127.0.0.1:1880/contatos"
-fetch(endpoint)
-.then(res => res.json())
-.then(res => {
-    console.log(res)
-})
+const btn_gravar = document.querySelector("#btn_gravar");
+const btn_cancelar = document.querySelector("#btn_cancelar");
+const f_nome = document.querySelector("#f_nome");
+const f_celular = document.querySelector("#f_celular");
+const f_email = document.querySelector("#f_email");
+const f_dtnasc = document.querySelector("#f_dtnasc");
+
+btn_gravar.addEventListener("click", (evt) => {
+  const valores = {
+    f_nome: f_nome.value,
+    f_celular: f_celular.value,
+    f_email: f_email.value,
+    f_dtnasc: f_dtnasc.value,
+  };
+
+  const cabecalho = {
+    method: 'POST',
+    dados: JSON.stringify(valores)
+  }
+
+  const endpoint = "http://127.0.0.1:1880/addcontatos";
+  fetch(endpoint, cabecalho)
+    .then((res) => {
+        if(res.status == 200){
+            console.log('OK')
+        } else {
+            alert("ERRO ao gravar contato.")
+        }
+    })
+
+});
