43104c8060b592aaa4c8b58b6e60c2ac909dc795
Rodrigo Cunha <rodrigocunha@live.com>
2024-07-22 22:53:32 -0300

    salvando

--- pesquisar.js
+++ pesquisar.js
@@ -1,41 +1,11 @@
-const btn_gravar = document.querySelector("#btn_gravar");
-const btn_cancelar = document.querySelector("#btn_cancelar");
-const f_nome = document.querySelector("#f_nome");
-const f_celular = document.querySelector("#f_celular");
-const f_email = document.querySelector("#f_email");
-const f_dtnasc = document.querySelector("#f_dtnasc");
+const btn_pesq = document.querySelector("#btn_pesq");
+const f_txtpesq = document.querySelector("#f_txtpesq");
 
-btn_gravar.addEventListener("click", (evt) => {
-  const dados = {
-    f_nome: f_nome.value,
-    f_celular: f_celular.value,
-    f_email: f_email.value,
-    f_dtnasc: f_dtnasc.value,
-  };
-
-  const cabecalho = {
-    method: "POST",
-    body: JSON.stringify(dados),
-  };
-
-  const endpoint = "http://127.0.0.1:1880/addcontatos";
-  fetch(endpoint, cabecalho).then((res) => {
-    if (res.status == 200) {
-      reset();
-    } else {
-      alert("ERRO ao gravar contato.");
+btn_pesq.addEventListener("click", (evt) => {
+    if(f_txtpesq.value == "") {
+        alert("Digite algo para pesquisar!")
+        f_txtpesq.focus()
+        return;
     }
-  });
-});
-
-btn_cancelar.addEventListener("click", (evt) => {
-  reset();
-});
-
-const reset = function () {
-  f_nome.value = "";
-  f_celular.value = "";
-  f_email.value = "";
-  f_dtnasc.value = "";
-  f_nome.focus();
-};
+    console.log(document.getElementsByName("f_pesq").value)
+})
