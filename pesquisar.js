c4d01df42a59293388fd3813d60cc0458e7a0339
Rodrigo Cunha <rodrigocunha@live.com>
2024-07-28 21:40:19 -0300

    criando o pesquisar

--- pesquisar.js
+++ pesquisar.js
@@ -2,10 +2,17 @@ const btn_pesq = document.querySelector("#btn_pesq");
 const f_txtpesq = document.querySelector("#f_txtpesq");
 
 btn_pesq.addEventListener("click", (evt) => {
-    if(f_txtpesq.value == "") {
-        alert("Digite algo para pesquisar!")
-        f_txtpesq.focus()
-        return;
-    }
-    console.log(document.getElementsByName("f_pesq").value)
-})
+  const valorpesq = f_txtpesq.value;
+  if (valorpesq == "") {
+    alert("Digite algo para pesquisar!");
+    f_txtpesq.focus();
+    return;
+  }
+  const f_pesq = document.querySelector("input[name=f_pesq]:checked").value;
+  const entpoint = `http://127.0.0.1:1880/pesquisarcontatos/${f_pesq}/${valorpesq}`;
+  fetch(entpoint)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+    });
+});
