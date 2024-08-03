00d5309bac36fe7ea7907a22ccf7cd5be17f8de9
Rodrigo Cunha <rodrigocunha@live.com>
2024-08-03 17:24:07 -0300

    finalizado css gestao

--- gestao.js
+++ gestao.js
@@ -1,51 +1,56 @@
 const dados = document.querySelector("#dados");
 
 const preencherdgv = () => {
-
-    dados.innerHTML = "";
-    const valorpesq = f_txtpesq.value;
-  if (valorpesq == "") {
-    alert("Digite algo para pesquisar!");
-    f_txtpesq.focus();
-    return;
-  }
-  const f_pesq = document.querySelector("input[name=f_pesq]:checked").value;
-  const entpoint = `http://127.0.0.1:1880/pesquisarcontatos/${f_pesq}/${valorpesq}`;
+  dados.innerHTML = "";
+  const entpoint = `http://127.0.0.1:1880/pesquisartodoscontatos`;
   fetch(entpoint)
     .then((res) => res.json())
     .then((res) => {
-      dados.innerHTML = ""; 
+      dados.innerHTML = "";
       res.forEach((el) => {
         const linha = document.createElement("div");
         linha.setAttribute("class", "linhadados");
 
-        const c1 = document.createElement('div');
+        const c1 = document.createElement("div");
         c1.setAttribute("class", "coluna c1");
         c1.innerHTML = el.n_contato_contato;
         linha.appendChild(c1);
 
-        const c2 = document.createElement('div');
+        const c2 = document.createElement("div");
         c2.setAttribute("class", "coluna c2");
         c2.innerHTML = el.s_nome_contato;
         linha.appendChild(c2);
 
-        const c3 = document.createElement('div');
+        const c3 = document.createElement("div");
         c3.setAttribute("class", "coluna c3");
         c3.innerHTML = el.s_celular_contato;
         linha.appendChild(c3);
 
-        const c4 = document.createElement('div');
+        const c4 = document.createElement("div");
         c4.setAttribute("class", "coluna c4");
         c4.innerHTML = el.s_email_contato;
         linha.appendChild(c4);
 
-        const c5 = document.createElement('div');
+        const c5 = document.createElement("div");
         c5.setAttribute("class", "coluna c5");
         c5.innerHTML = el.dt_dtnasc_contato;
         linha.appendChild(c5);
 
+        const c6 = document.createElement("div");
+        c6.setAttribute("class", "coluna c6 c_op");
+        const imgdelete = document.createElement("img");
+        imgdelete.setAttribute("src", "./delete.svg" )
+        imgdelete.setAttribute("class", "iconeop" )
+        const imgeditar = document.createElement("img");
+        imgeditar.setAttribute("src", "./edit.svg")
+        imgeditar.setAttribute("class", "iconeop")
+        c6.appendChild(imgdelete)
+        c6.appendChild(imgeditar)
+        linha.appendChild(c6);
+
         dados.appendChild(linha);
-      })
-      
+      });
     });
 };
+
+preencherdgv();
